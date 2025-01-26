c5127ef3a2e8771f86daff372d6e88b7fa4e8254
Zac <ztaylor555@gmail.com>
2025-01-26 11:25:28 -0500

    improvements

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="container">
+  <div @keyup.space="increaseHandTotal" class="container">
     <p class="is-size-2">Total hands: {{ totalHands }}</p>
-    <button class="hand-total button is-primary" @click="increaseHandTotal">
+    <button
+      :disabled="!buttonIsInteractable"
+      class="hand-total button is-primary"
+      @click="increaseHandTotal"
+    >
       + Hand
     </button>
     <div class="columns is-multiline">
@@ -10,19 +14,41 @@
           :playerId="index + 1"
           :totalHands="totalHands"
           :handsSeen="handsSeenByPlayer"
-          @clearStats="clearStats"
+          @checkInteractable="checkInteractable"
         />
       </div>
     </div>
+
+    <button class="hand-total button is-primary" @click="increaseHandTotal">
+      Force inc
+    </button>
   </div>
 </template>
 
 <script setup>
 const totalHands = ref(0);
 const totalPlayers = ref(9);
+const buttonIsInteractable = ref(true);
+
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+      increaseHandTotal();
+    }
+  });
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keyup");
+});
 
 const increaseHandTotal = () => {
   totalHands.value += 1;
+  buttonIsInteractable.value = false;
+};
+
+const checkInteractable = () => {
+  buttonIsInteractable.value = true;
 };
 </script>
 
--- components/StatController.vue
+++ components/StatController.vue
@@ -1,5 +1,8 @@
 <template>
-  <div :class="`card has-text-white has-background-${getRangeColor}`">
+  <div
+    @keyup.esc="flipCard"
+    :class="`card has-text-white has-background-${getRangeColor}`"
+  >
     <div class="container">
       <h2 class="is-size-3">Player {{ playerId }} : {{ getStyle }}</h2>
 
@@ -9,16 +12,54 @@
       <button class="button is-danger" @click="increasePfr">
         + Preflop Raise
       </button>
-      <p class="is-size-5">
-        vpip: <span class="has-text-weight-bold">{{ calcVpip }}%</span>
-      </p>
+      <div class="card-content">
+        <div class="columns">
+          <div class="column">
+            <p class="is-size-5">
+              vpip: <span class="has-text-weight-bold">{{ calcVpip }}%</span>
+            </p>
+            <p class="has-text-white is-size-5">
+              PFR: <span class="has-text-weight-bold">{{ calcPfr }}%</span>
+            </p>
+          </div>
+          <div class="column">
+            <p>Hands played: {{ playedHands }} / {{ handsSeen }}</p>
+            <p>Hands raised: {{ preFlopRaises }} / {{ playedHands }}</p>
+          </div>
+        </div>
+      </div>
 
-      <p class="has-text-white is-size-5">
-        PFR: <span class="has-text-weight-bold">{{ calcPfr }}%</span>
-      </p>
-      <p>Hands opponent has played: {{ playedHands }} / {{ handsSeen }}</p>
-      <p>Hands opponent has raised: {{ preFlopRaises }} / {{ playedHands }}</p>
-      <button @click="clearStats" class="button is-danger">Clear Data</button>
+      <div class="columns">
+        <div class="column">
+          <button @click="flipCard" class="button is-info">Flip</button>
+        </div>
+        <div class="column">
+          <button @click="clearStats" class="button is-danger">
+            Clear Data
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div :class="`modal ${isFlipped ? 'is-active' : ''}`">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="columns">
+          <div class="column">
+            Raising Range
+            <img :src="currentRange.raise" alt="" />
+          </div>
+          <div class="column">
+            Calling Range
+            <img :src="currentRange.call" alt="" />
+          </div>
+        </div>
+      </div>
+      <button
+        @click="flipCard"
+        class="modal-close is-large"
+        aria-label="close"
+      ></button>
     </div>
   </div>
 </template>
@@ -34,11 +75,46 @@ const props = defineProps({
     default: 0,
   },
 });
+
 const vPip = ref(0);
 const pfr = ref(0);
 const playedHands = ref(0);
 const preFlopRaises = ref(0);
 const handsSeen = ref(0);
+const emit = defineEmits(["checkInteractable"]);
+const opponentCalled = ref(false);
+const opooonentRaised = ref(false);
+const isFlipped = ref(false);
+
+const rangeData = {
+  nit: {
+    raise:
+      "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/9-Percent-Range.jpg",
+    call: "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/8-Calling-Range.jpg",
+  },
+  rock: {
+    raise:
+      "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/15-Percent-Range.jpg",
+    call: "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/16-Calling-Range.jpg",
+  },
+  lag: {
+    raise:
+      "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/50-Percent-Range.jpg",
+    call: "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/30-Calling-Range.jpg",
+  },
+  tag: {
+    raise:
+      "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/20-Percent-Range.jpg",
+    call: "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/16-Calling-Range.jpg",
+  },
+  passive: {
+    raise:
+      "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/20-Percent-Range.jpg",
+    call: "https://www.splitsuit.com/nitropack_static/yYipRtHrrGgKGqoGltBMbGTmtbORZOjD/assets/images/optimized/rev-ba6dbfa/www.splitsuit.com/wp-content/uploads/2020/02/50-Percent-Range.jpg",
+  },
+};
+
+const currentRange = ref({});
 
 const clearStats = () => {
   handsSeen.value = 0;
@@ -48,83 +124,126 @@ const clearStats = () => {
   preFlopRaises.value = 0;
 };
 
+const flipCard = () => {
+  isFlipped.value = !isFlipped.value;
+};
+
 watch(
   () => props.totalHands,
   (newValue, oldValue) => {
-    // This watcher is triggered whenever 'myProp' changes
     handsSeen.value += 1;
+    opooonentRaised.value = false;
+    opponentCalled.value = false;
   }
 );
+
+const checkInteractable = () => {
+  emit("checkInteractable");
+};
 
 const increaseHand = () => {
   if (playedHands.value < handsSeen.value) {
     playedHands.value += 1;
+    checkInteractable();
+    opponentCalled.value = true;
   }
 };
 
 const increasePfr = () => {
-  if (preFlopRaises.value < playedHands.value) preFlopRaises.value += 1;
+  if (preFlopRaises.value < playedHands.value) {
+    opponentCalled.value = false;
+    opooonentRaised.value = true;
+    preFlopRaises.value += 1;
+    checkInteractable();
+  }
 };
 
 const calcVpip = computed(() => {
   vPip.value = (playedHands.value / handsSeen.value) * 100;
 
-  if (vPip.value == Infinity) {
+  if (Number.isNaN(vPip.value)) {
     return 0;
   } else {
-    return vPip.value;
+    return Math.round(vPip.value);
   }
 });
 
 const calcPfr = computed(() => {
   pfr.value = (preFlopRaises.value / playedHands.value) * 100;
 
-  if (pfr.value === NaN) {
+  if (Number.isNaN(pfr.value)) {
     return 0;
   } else {
-    return pfr.value;
+    return Math.round(pfr.value);
   }
 });
 
 const getStyle = computed(() => {
-  if (isPassive()) return "Passive";
-  if (isRock()) return "ROCK";
-  if (isNit()) return "NIT";
-  if (isVeryPassive()) return "Very Passive";
-  if (isLooseAggressive()) return "Loose Aggressive";
+  if (isPassive()) {
+    currentRange.value = rangeData["passive"];
+
+    return "Passive";
+  }
+  if (isRock()) {
+    currentRange.value = rangeData["rock"];
+
+    return "ROCK";
+  }
+  if (isNit()) {
+    currentRange.value = rangeData["nit"];
+    return "NIT";
+  }
+  if (isVeryPassive()) {
+    currentRange.value = rangeData["passive"];
+
+    return "Very Passive";
+  }
+  if (isLooseAggressive()) {
+    currentRange.value = rangeData["lag"];
+
+    return "Loose Agg";
+  }
+  if (isTightAggressive()) {
+    currentRange.value = rangeData["tag"];
+
+    return "Tight Agg";
+  }
 });
 
-const isLooseAggressive = () => {
-  return vPip.value >= 60 && pfr.value >= 50;
-};
-
 const isVeryPassive = () => {
-  return vPip.value >= 60 && pfr.value <= 10;
+  return vPip.value >= 50 && pfr.value <= 30;
 };
 
 const isPassive = () => {
-  return vPip.value >= 40 && pfr.value <= 15;
+  return vPip.value >= 35 && vPip.value < 50 && pfr.value <= 30;
 };
+
+const isLooseAggressive = () => {
+  return vPip.value >= 25 && pfr.value > 30;
+};
+
 const isRock = () => {
-  return vPip.value < 25 && pfr.value <= 15;
+  return vPip.value >= 15 && vPip.value <= 30 && pfr.value <= 30;
 };
 
 const isNit = () => {
-  return vPip.value <= 15 && pfr.value <= 15 && pfr.value >= 6;
+  return vPip.value < 15 && pfr.value <= 15;
 };
 
 const isTightAggressive = () => {
-  return vPip.value < 25 && pfr.value >= 50;
+  return vPip.value <= 25 && pfr.value >= 50;
 };
+
+const getGraphData = computed(() => {});
 
 const getRangeColor = computed(() => {
   let colorClass = "primary-dark";
   if (isPassive()) colorClass = "primary";
   if (isVeryPassive()) colorClass = "success";
-  //   if (isNit()) colorClass = "link";
-  if (isRock()) colorClass = "text-dark";
   if (isLooseAggressive()) colorClass = "danger-dark";
   if (isTightAggressive()) colorClass = "warning";
+  if (isRock()) colorClass = "text-dark";
+  if (isNit()) colorClass = "text";
 
   return colorClass;
 });
@@ -135,7 +254,20 @@ const getRangeColor = computed(() => {
   padding: 0 2rem;
 }
 
+.modal-content {
+  width: 100% !important;
+}
+
 .button {
   margin: 0 0.5rem;
+}
+
+.card-content,
+.modal img {
+  padding: 2rem 0;
+}
+
+img {
+  max-width: 500px;
 }
 </style>
